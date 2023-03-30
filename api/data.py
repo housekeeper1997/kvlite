d48e879c7063b932a4963246ae72d4bfb0e16254
windsting <windsting999@gmail.com>
2023-03-30 23:53:08 +0700

    use mysql instead sqlite as data storage

--- api/data.py
+++ api/data.py
@@ -1,86 +1,102 @@
 #! /usr/bin/env python3
 
-import sqlite3
 import unittest
 import os
+import sqlalchemy as db
+from sqlalchemy import Column, String, Integer
+from api.utility import env_str, env_int
 
-KVTABLE_NAME = os.getenv('KVTABLE_NAME','BanData')
-COLUMN_PREFIX = os.getenv('COLUMN_PREFIX','tbt')
+KVTABLE_NAME = env_str('KVTABLE_NAME','BanData')
+COLUMN_PREFIX = env_str('COLUMN_PREFIX','tbt')
+KEY_COLUMN_LENGTH = env_int('KEY_COLUMN_LENGTH', 256)
+VALUE_COLUMN_LENGTH = env_int('VALUE_COLUMN_LENGTH', 1024*8)
+UPDATE_COLUMN_LENGTH = env_int('UPDATE_COLUMN_LENGTH', 256)
 KCOLNAME = f'{COLUMN_PREFIX}_key'
 VCOLNAME = f'{COLUMN_PREFIX}_value'
 UCOLNAME = f'{COLUMN_PREFIX}_update'
 
 def connect_db(dbname, clean=False):
-    conn = sqlite3.connect(dbname)
-    if clean:
-        conn.execute(f'DROP TABLE IF EXISTS {KVTABLE_NAME};')
-    query = f'''CREATE TABLE IF NOT EXISTS {KVTABLE_NAME} (
-                {KCOLNAME} text NOT NULL UNIQUE,
-                {VCOLNAME} text NOT NULL,
-                {UCOLNAME} text NOT NULL
-            );'''
-    conn.execute(query)
-    # print("Table created successfully")
-    return conn
+    # conn = sqlite3.connect(dbname)
+    engine = db.create_engine(dbname, echo=False)
+    with engine.connect() as conn:
+        if clean:
+            qdrop = f'DROP TABLE IF EXISTS {KVTABLE_NAME};'
+            conn.execute(db.text(qdrop))
+            conn.commit()
+        meta = db.MetaData()
+        table = db.Table(
+                KVTABLE_NAME, meta, 
+                Column('rowid', Integer, primary_key = True), 
+                Column(KCOLNAME, String(KEY_COLUMN_LENGTH)), 
+                Column(VCOLNAME, String(VALUE_COLUMN_LENGTH)), 
+                Column(UCOLNAME, String(UPDATE_COLUMN_LENGTH)), 
+                mysql_charset='utf8mb4',
+            )
+        meta.create_all(engine)
+        # query = f'''CREATE TABLE IF NOT EXISTS {KVTABLE_NAME} (
+        #             {KCOLNAME} text NOT NULL UNIQUE,
+        #             {VCOLNAME} text NOT NULL,
+        #             {UCOLNAME} text NOT NULL
+        #         );'''
+        # conn.execute(db.text(query))
+        return engine
 
-def insert(conn, k, v, now, tn=KVTABLE_NAME, kcn=KCOLNAME, vcn=VCOLNAME, ucn=UCOLNAME):
-    cur = conn.cursor()
-    qinsert = f"INSERT INTO {tn}({kcn},{vcn},{ucn}) values(?, ?, ?)"
-    cur.execute(qinsert,(k,v,now))
-    cur.close()
-    conn.commit()
-    # print(f"lastrowid: {cur.lastrowid}")
-    return cur.lastrowid != 0, cur
+def insert(engine, k, v, now, tn=KVTABLE_NAME, kcn=KCOLNAME, vcn=VCOLNAME, ucn=UCOLNAME):
+    stmt = f"INSERT INTO {tn}({kcn},{vcn},{ucn}) values(:k, :v, :now)"
+    values = {'v': v,'now': now,'k': k}
+    with engine.begin() as conn:
+        conn.execute(db.text(stmt), values)
+        # conn.commit()
+        meta = db.MetaData()
+        result = conn.execute(db.text("SELECT LAST_INSERT_ID()"))
+        lastrowid = result.fetchone()[0]
+        return lastrowid != 0, lastrowid
 
-def update(conn, k, v, now, tn=KVTABLE_NAME, kcn=KCOLNAME, vcn=VCOLNAME, ucn=UCOLNAME):
-    # print(f'gonna update key="{k}" with value="{v}"')
-    qupdate = f"update {tn} set {vcn}=?,{ucn}=? where {kcn}=?"
-    cur = conn.cursor()
-    cur.execute(qupdate,[v,now,k])
-    # print(cur.rowcount)
-    cur.close()
-    conn.commit()
-    return cur.rowcount == 1, cur
+def update(engine, k, v, now, tn=KVTABLE_NAME, kcn=KCOLNAME, vcn=VCOLNAME, ucn=UCOLNAME):
+    stmt = f'UPDATE {tn} SET {vcn} = :v, {ucn} = :now WHERE {kcn}  = :k'
+    values = {'v': v,'now': now,'k': k}
+    with engine.begin() as conn:
+        conn.execute(db.text(stmt), values)
 
-def select(conn, k, tn=KVTABLE_NAME, kcn=KCOLNAME, vcn=VCOLNAME, ucn=UCOLNAME):
-    qselect = f"select {kcn}, {vcn}, {ucn} from {tn} where {kcn}=?"
-    qselect = f"select * from {tn} where {kcn}=?"
-    cur = conn.cursor()
-    row = cur.execute(qselect, (k,)).fetchone()
-    # print(row)
-    return row
+def select(engine, k, tn=KVTABLE_NAME, kcn=KCOLNAME, vcn=VCOLNAME, ucn=UCOLNAME):
+    stmt = f"select {kcn}, {vcn}, {ucn} from {tn} where {kcn}= :k"
+    values = {'k': k}
+    with engine.begin() as conn:
+        result = conn.execute(db.text(stmt), values)
+        row = result.fetchone()
+        # print(row)
+        if row is None:
+            return None
+        return row
 
-def selectAll(conn, tn=KVTABLE_NAME):
-    qselect = f"select * from {tn}"
-    cur = conn.cursor()
-    cur.execute(qselect)
-    rows = cur.fetchall()
-    # print(rows)
-    cur.close()
-    return rows
+def selectAll(engine, tn=KVTABLE_NAME, kcn=KCOLNAME, vcn=VCOLNAME, ucn=UCOLNAME):
+    metadata = db.MetaData()
+    stmt = f"select {kcn}, {vcn}, {ucn} from {tn}"
+    with engine.connect() as conn:
+        result = conn.execute(db.text(stmt))
+        return result
 
-def rowCount(conn, tn=KVTABLE_NAME):
-    qselect = f"select count(rowid) from {tn}"
-    cur = conn.cursor()
-    cur.execute(qselect)
-    count = cur.fetchone()[0]
-    # print(f"count is: {count}")
-    return count
+def rowCount(engine, tn=KVTABLE_NAME):
+    stmt = f"select count(rowid) from {tn}"
+    with engine.begin() as conn:
+        result = conn.execute(db.text(stmt)).fetchone()[0]
+        # print(result)
+        return result
 
-def delete(conn, k=None, tn=KVTABLE_NAME, kcn=KCOLNAME):
+def delete(engine, k=None, tn=KVTABLE_NAME, kcn=KCOLNAME):
+    stmt = f"delete from {tn} where {kcn}= :k"
     if k is None:
-        qdelete = f"delete from {tn}"
-        conn.execute(qdelete)
-    else:
-        qdelete = f"delete from {tn} where {kcn}=?"
-        conn.execute(qdelete, (k,))
-    conn.commit()
+        stmt = f"delete from {tn}"
+    values = {'k': k}
+    with engine.begin() as conn:
+        conn.execute(db.text(stmt), values)
 
-def getRowCount(conn, tn=KVTABLE_NAME, kcn=KCOLNAME):
+def getRowCount(engine, tn=KVTABLE_NAME, kcn=KCOLNAME):
     qselect = f"select count({kcn}) from {tn}"
-    cur = conn.cursor()
-    cur.execute(qselect)
-    return cur.fetchone()[0]
+    with engine.begin() as conn:
+        result = conn.execute(db.text(qselect)).fetchone()[0]
+        # print(result)
+        return result
 
 def getTableinfo(connection, tn=KVTABLE_NAME):
     qtableinfo = f"PRAGMA table_info('{tn}');"
@@ -89,52 +105,56 @@ def getTableinfo(connection, tn=KVTABLE_NAME):
     cur.close()
     print(tableinfo)
 
-TESTDBNAME = ":memory:"
+TESTDBNAME = "sqlite:///:memory:"
+# TESTDBNAME = 'sqlite:///KVData.db'
 TESTNOW = "now"
 class TestDbOperations(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        cls.conn = connect_db(TESTDBNAME, True)
+        cls.engine = connect_db(TESTDBNAME, True)
 
     @classmethod
     def tearDownClass(cls):
-        cls.conn.close()
+        pass
+        # cls.engine.close()
 
     def test_empty(self):
-        rows = selectAll(self.conn)
+        rows = selectAll(self.engine)
         self.assertEqual(len(rows), 0, "Got content from empty table")
 
     def test_insert(self):
-        _, cur = insert(self.conn,'testkey01','["testjsondata1"]', TESTNOW)
-        self.assertNotEqual(cur.lastrowid, 0, "Insert operation failed")
+        _, lastrowid = insert(self.engine,'testkey01','["testjsondata1"]', TESTNOW)
+        self.assertNotEqual(lastrowid, 0, "Insert operation failed")
     
     def test_inserted_select(self):
         value_inserted = '["new testjsondata2"]'
-        insert(self.conn,'testkey02', value_inserted, TESTNOW)
-        data = select(self.conn,'testkey02')
+        insert(self.engine,'testkey02', value_inserted, TESTNOW)
+        data = select(self.engine,'testkey02')
         # print(data)
         self.assertEqual(data[1], value_inserted, "Got wrong value")
 
     def test_inserted_update(self):
-        _, cur = update(self.conn,'testkey01',TestJson, TESTNOW)
-        self.assertEqual(cur.rowcount, 1, f"Update opreation failed with rowcount: {cur.rowcount}")
+        k = 'testkey01'
+        update(self.engine, k,TestJson, TESTNOW)
+        row = select(self.engine, k)
+        self.assertEqual(row[1], TestJson, f"value is not we set")
 
     def test_nonexist_select(self):
-        row = select(self.conn,'testkey05')
+        row = select(self.engine,'testkey05')
         # print(f"row: {row}")
         self.assertEqual(row, None, "Got value should be empty")
 
     def test_row_delete(self):
-        # rows = selectAll(self.conn)
+        # rows = selectAll(self.engine)
         # print(f"rows before delete: {rows}")
-        rowCountBefore = getRowCount(self.conn)
-        delete(self.conn, 'testkey01')
-        rowCountAfter = getRowCount(self.conn)
+        rowCountBefore = getRowCount(self.engine)
+        delete(self.engine, 'testkey01')
+        rowCountAfter = getRowCount(self.engine)
         self.assertEqual(rowCountAfter, rowCountBefore-1, "row count wrong after delete")
     
     def test_total_clean(self):
-        delete(self.conn)
-        rowCountAfter = getRowCount(self.conn)
+        delete(self.engine)
+        rowCountAfter = getRowCount(self.engine)
         self.assertEqual(rowCountAfter, 0, "table should be empty after delete all")
 
 TestJson = '{"BanList":"11点下线,psi,qazqaz,SRX-ATX,纣王","ShowBanTip":true,"BanTip":"Blocked!!!!!","BanNegJisao":false,"JisaoMin":0,"BanQuote":true}'
@@ -155,5 +175,5 @@ if __name__ == "__main__":
     # print("gonna delete all rows")
     # delete(connection)
     # print(f"final row count: {getRowCount(connection)}")
-    # connection.close()
+    # # connection.close()
     unittest.main()
--- api/index.py
+++ api/index.py
@@ -6,11 +6,12 @@ from time import time
 
 from api.utility import env_float, env_int, env_str, failed, strExceedLimit, succeed
 
-DB_NAME = env_str('DB_NAME','KVData.db')
+# DB_NAME = env_str('DB_NAME','KVData.db')
+DB_NAME = env_str('DB_NAME','sqlite:///KVData.db')
 TZ_HOUR_OFFSET = env_float('TZ_HOUR_OFFSET', 8.0)    # Bejing Standard Time (UTC+08:00)
 ROW_COUNT_LIMIT = env_int('ROW_COUNT_LIMIT',1000)
 KEY_LENGTH_LIMIT = env_int('KEY_LENGTH_LIMIT', 64)
-VALUE_LENGTH_LIMIT = env_int('VALUE_LENGTH_LIMIT', 1024 * 10)                   # bytes
+VALUE_LENGTH_LIMIT = env_int('VALUE_LENGTH_LIMIT', 1024 * 8)                   # bytes
 MINIMUM_SET_INTERVAL_PER_USER = env_int('MINIMUM_SET_INTERVAL_PER_USER', 60)    # seconds
 MINIMUM_SET_INTERVAL_SYSTEM = env_int('MINIMUM_SET_INTERVAL_SYSTEM', 1)         # seconds
 
@@ -22,30 +23,51 @@ LastWriteTime = timepoint.timepoint()
 
 app = Flask(__name__)
 
+DbEngine = None
+
+def get_engine():
+    global DbEngine
+    if DbEngine is None:
+        DbEngine = db.connect_db(DB_NAME)
+    return DbEngine
+
+
 @app.route('/')
 def hello():
-    return '''<div style="height:100%;display:flex;align-items:center;justify-content:center;">
-                <div style="margin:0;font-size:2rem;">
-                    <em>Hello World!</em> -- with <code>Flask</code> and <code>SQLite</code>
+    return '''<html>
+            <body style="background-color:#f7f7f7;color:#555;font-family: Söhne,ui-sans-serif,system-ui,-apple-system,Segoe UI,Roboto,Ubuntu,Cantarell,Noto Sans,sans-serif,Helvetica Neue,Arial,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol,Noto Color Emoji;">
+            <div style="height:100%;display:flex;flex-direction:column;align-items:center;justify-content:center;margin-left:-5em;">
+                <div style="margin:.8em;margin-top:-3em;font-size:2rem;text-align:center;">
+                    <p style="margin:auto;"><strong>KVLite</strong></p>
+                    <p style="margin:auto;margin-top:.3em;padding-left:12em;font-size:.65em;">-- A key-value storage service.</p>
                 </div>
-            </div>\n'''
+                <div style="margin:0;font-size:1.3em;">
+                    Created with:
+                    <ul style="margin:auto;">
+                        <li><code>python</code></li>
+                        <li><code>Flask</code></li>
+                        <li><code>SQLAlchemy</code></li>
+                    </ul>
+                </div>
+            </div>
+            </body>
+            </html>\n'''
 
 @app.route('/kv')
 def get_data():
-    with db.connect_db(DB_NAME) as conn:
-        return jsonify(db.selectAll(conn))
+    rows = db.selectAll(get_engine())
+    rows = [{KKEY: r[0],VKEY: r[1],TKEY: r[2]} for r in rows]
+    return jsonify(rows)
 
 @app.route('/kv/<key>')
 def get_kv(key):
     # print(f"got request for key: {key}")
-    with db.connect_db(DB_NAME) as conn:
-        row = db.select(conn, key)
-        # print(f"got result: {row}")
-        if row is None:
-            result = failed( 'data not exist')
-            # print(f"gonna return {result}")
-            return result
-        return {KKEY:row[0], VKEY:row[1], TKEY:row[2]}
+    row = db.select(get_engine(), key)
+    if row is None:
+        return failed( 'data not exist')
+    value = {KKEY:row[0], VKEY:row[1], TKEY:row[2]}
+    # print(value)
+    return value
 
 @app.route('/kv', methods=['POST'])
 def set_kv():
@@ -57,21 +79,21 @@ def set_kv():
         return failed(f'key length exceed {KEY_LENGTH_LIMIT} bytes')
     if strExceedLimit(kv[VKEY], VALUE_LENGTH_LIMIT):
         return failed(f'data length exceed limit: {VALUE_LENGTH_LIMIT} bytes')
-    with db.connect_db(DB_NAME) as conn:
-        oldVal = db.select(conn,kv[KKEY])
-        if oldVal is not None:
-            userLastWriteTime = timepoint.timepoint(float(oldVal[2]))
-            if userLastWriteTime.elapsed() < MINIMUM_SET_INTERVAL_PER_USER:
-                return failed('wait one minute')
-            db.update(conn, kv[KKEY], kv[VKEY], str(time()))
-            LastWriteTime.set()
-            return succeed('updated')
-        if db.rowCount(conn) > ROW_COUNT_LIMIT:
-            print(f"table row count reach limit: {ROW_COUNT_LIMIT}")
-            return failed('can not create more rows')
+    oldVal = db.select(get_engine(),kv[KKEY])
+    # print(oldVal)
+    if oldVal is not None:
+        userLastWriteTime = timepoint.timepoint(float(oldVal[2]))
+        if userLastWriteTime.elapsed() < MINIMUM_SET_INTERVAL_PER_USER:
+            return failed('wait one minute')
+        db.update(get_engine(), kv[KKEY], kv[VKEY], str(time()))
         LastWriteTime.set()
-        db.insert(conn, kv[KKEY], kv[VKEY], str(time()))
-        return succeed('created')
+        return succeed('updated')
+    if db.rowCount(get_engine()) > ROW_COUNT_LIMIT:
+        print(f"table row count reach limit: {ROW_COUNT_LIMIT}")
+        return failed('can not create more rows')
+    LastWriteTime.set()
+    db.insert(get_engine(), kv[KKEY], kv[VKEY], str(time()))
+    return succeed('created')
 
 if __name__ == "__main__":
     from waitress import serve
